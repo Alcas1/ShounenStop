c3cb2fb0fc65911e172f7d180283e06de917cfc6
Jonathan Wu <jonathanwu70@gmail.com>
2021-09-07 11:42:42 -0400

    remove breaking script

--- scripts/RemoveEvent.py
+++ scripts/RemoveEvent.py
@@ -1,14 +1,6 @@
 import os
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 eventId = 'c-1xqKj5UJA-c'
-=======
-eventId = 'c-vq3UkDleN-c'
->>>>>>> parent of b4f4d891... remove Air Comic Treasure (Current)
-=======
-eventId = 'c-c3dcWkXD2-c'
->>>>>>> parent of fff9bafa... remove Air Comiket Summer
 
 comiketDir = "content/comiket"
 nonEventReferencedImages = []

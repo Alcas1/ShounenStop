19a8cc85a9f9d1f97ebf9ad4c86f03fb00c973ab
Jonathan Wu <jonathanwu70@gmail.com>
2021-10-01 00:00:05 -0400

    update paypal link

--- src/components/Payment/PaymentContainer.js
+++ src/components/Payment/PaymentContainer.js
@@ -278,7 +278,7 @@ class PaymentContainer extends React.Component {
                                   this.sendCheckoutData(context)
                                   this.startValidatingPayment(context)
                                   const paypalLink =
-                                    'https://www.paypal.com/paypalme/LeonShum/' +
+                                    'https://www.paypal.com/paypalme/rebjshum/' +
                                     this.props.orderContext.totalPrice.toFixed(
                                       2
                                     ) +
